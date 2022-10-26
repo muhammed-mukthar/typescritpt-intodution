b2240295f01820de5b290b477e4c1c85e004e336
Muhammed Mukthar <muktharmanaf28@gmail.com>
2022-10-26 10:31:12 +0530

    error handeling

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,52 @@
-import express from 'express'
+import express,{NextFunction, Request,Response} from 'express'
 const app=express()
 
-app.get('/',(req,res)=>{
-    return res.send('hallo')
-})
 
 app.use(express.json())
 
 app.use(express.urlencoded())
 
-app.post('/api/data',(req,res)=>{
-    console.log(req.body);
-    return res.sendStatus(200)
+const middleware=({name}:{name:string})=>(req:Request,res:Response,next:NextFunction)=>{
+ res.locals.name=name
+
+next()
+}
+
+
+
+
+app.use(middleware({name:"mukthar"}))
+
+
+app.get('/api/books/:bookId/:authId',
+(req:Request<{bookId:'string',authId:string},{},{name:string},{}>,res:Response,next:NextFunction)=>{
+
+req.body.name
+
+    console.log(res.locals.name);
+
+     res.send(res.locals.name);
+
+    
+});
+
+async function throwsError(){
+ throw new Error('Boom')
+}
+
+app.get('/error',async(req,res)=>{
+    try{
+ await throwsError()
+ res.sendStatus(300)
+  
+    
+    }catch(e){
+  res.status(400).send('spmething bad happened')
+    }
+  
+   
 })
 
 app.listen(3000,()=>{console.log('running on 3000');
 })
+
